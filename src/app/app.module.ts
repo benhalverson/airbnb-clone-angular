7e1310aceec9b7796fdc28c3b0de63494baf973c
Ben Halverson <benhalverson33@gmail.com>
2019-08-26 21:39:33 -0700

    wip still :-(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
@@ -14,27 +14,24 @@ import { ManageModule } from './manage/manage.module';
 import { UserModule } from './user/user.module';
 
 const routes: Routes = [
-  // {path: '', component: RentalComponent}
-  { path: '', redirectTo: '/rentals', pathMatch: 'full'}
-]
+	// {path: '', component: RentalComponent}
+	{ path: '', redirectTo: '/rentals', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    RentalModule,
-    AuthModule,
-    NgbModule.forRoot(),
-    ToastrModule.forRoot(),
-    BrowserAnimationsModule,
-    ManageModule,
-    UserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HeaderComponent ],
+	imports: [
+		RouterModule.forRoot(routes),
+		BrowserModule,
+		RentalModule,
+		AuthModule,
+		NgbModule.forRoot(),
+		ToastrModule.forRoot(),
+		// BrowserAnimationsModule,
+		ManageModule,
+		UserModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
